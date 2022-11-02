dfa2601ec877697f7718737f14d9befe110fc17e
flavia borelly <flavia.borelly987@gmail.com>
2022-11-02 13:16:46 -0500

    agregando grilla para mostrando los producto y la card de los productos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import CardProduct from "./CardProduct";
 
 function App() {
+  const [product, setproduct] = useState([{ id: 1 }, { id: 2 }, { id: 3 }]);
+  const [pagina, setpagina] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div>
+          <h1 id="container" style={{ color: "red" }}>
+            Pagina # {pagina}
+          </h1>
+        </div>
+
+        <div>
+          {/* <Paginations setpage={setpage} page={page} /> */}
+          PAGINACION
+        </div>
+      </div>
+
+      <div className="container">
+        {product.length !== 0 &&
+          product.map((data) => (
+            <div>
+              <CardProduct key={data.id} data={data} />
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
 
